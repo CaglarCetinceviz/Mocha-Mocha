b6e5f984453482f856d95737c5b18cbfff920662
Caglar Cetinceviz <caglarcetinceviz@gmail.com>
2025-01-27 14:51:17 +0000

    main, menu and order page finished

---
+++ app/src/main/java/com/example/mochamocha/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.mochamocha;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.mochamocha.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()) {
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.menu:
+                    replaceFragment(new MenuFragment());
+                    break;
+                case R.id.order:
+                    replaceFragment(new OrderFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+    }
+
+//            EdgeToEdge.enable(this);
+//    setContentView(R.layout.activity_main);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//        Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//        v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//        return insets;
+//    });
+}
